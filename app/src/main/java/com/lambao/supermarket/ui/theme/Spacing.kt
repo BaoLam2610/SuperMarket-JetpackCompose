33115d195592f5462d53762c2fbeacb3e8e96a41
lamnb <lamnb.paraline@gmail.com>
2023-09-28 16:17:15 +0700

    [config] - #2. add config color, spacing, text style

---
+++ app/src/main/java/com/lambao/supermarket/ui/theme/Spacing.kt
@@ -0,0 +1,52 @@
+package com.lambao.supermarket.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/* Common size */
+object Size {
+    val tiny = 2.dp
+    val extraTiny = 3.dp
+    val small = 4.dp
+    val extraSmall = 8.dp
+    val normal = 10.dp
+    val extraNormal = 12.dp
+    val medium = 16.dp
+    val extraMedium = 20.dp
+    val large = 32.dp
+    val extraLarge = 64.dp
+}
+
+/* Font size */
+object Font {
+    val small = 4.sp
+    val extraSmall = 8.sp
+    val normal = 10.sp
+    val extraNormal = 12.sp
+    val default = 14.sp
+    val medium = 16.sp
+    val extraMedium = 18.sp
+    val large = 20.sp
+    val extraLarge = 24.sp
+}
+
+/* Line size*/
+object Line {
+    val tiny = 0.5.dp
+    val extraTiny = 0.8.dp
+    val normal = 1.dp
+    val medium = 1.2.dp
+    val large = 2.dp
+}
+
+/* Icon size*/
+object Icon {
+    val small = 14.dp
+    val extraSmall = 16.dp
+    val normal = 18.dp
+    val extraNormal = 20.dp
+    val medium = 24.dp
+    val extraMedium = 28.dp
+    val large = 32.dp
+    val extraLarge = 64.dp
+}
---
+++ app/src/main/java/com/lambao/supermarket/ui/theme/TextStyle.kt
@@ -0,0 +1,42 @@
+package com.lambao.supermarket.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+
+object TextStyle {
+    val Regular10 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = Font.normal,
+        color = ColorText
+    )
+
+    val Regular12 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = Font.extraNormal,
+        color = ColorText
+    )
+
+    val Regular14 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = Font.default,
+        color = ColorText
+    )
+
+    val Bold10 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = Font.normal,
+        color = ColorText
+    )
+
+    val Bold12 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = Font.extraNormal,
+        color = ColorText
+    )
+
+    val Bold14 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = Font.default,
+        color = ColorText
+    )
+}
