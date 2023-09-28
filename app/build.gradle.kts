04a31f9a81318e63893c3c1da3e5e550f9497b34
lamnb <lamnb.paraline@gmail.com>
2023-09-28 15:44:23 +0700

    [config] - #1. add config libs, build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
     namespace = "com.lambao.supermarket"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.lambao.supermarket"
@@ -30,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -45,25 +48,47 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/**/*"
         }
+    }
+    applicationVariants.all {
+        addJavaSourceFoldersToModel(
+            File(buildDir, "generated/ksp/$name/kotlin")
+        )
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.android.core.ktx)
+    implementation(libs.android.lifecycle.runtime.ktx)
+    implementation(libs.android.lifecycle.runtime.compose)
+    implementation(libs.android.lifecycle.viewmodel.ktx)
+    implementation(libs.android.lifecycle.viewmodel.compose)
+    implementation(libs.android.activity.compose)
+
+    val composeBom = platform(libs.android.compose.bom)
+    implementation(composeBom)
+    implementation(libs.bundles.compose.libs)
+    implementation(libs.android.compose.coil)
+    implementation(libs.android.compose.constraintlayout)
+    implementation(libs.android.compose.destinations.core)
+    ksp(libs.android.compose.destinations.ksp)
+
+    implementation(libs.dagger.hilt.android)
+    implementation(libs.dagger.hilt.android.compiler)
+    implementation(libs.android.hilt.compiler)
+    implementation(libs.android.hilt.navigation.compose)
+
+    implementation(libs.retrofit2.core)
+    implementation(libs.retrofit2.converter.gson)
+    implementation(libs.okhttp3.logging.interceptor)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(composeBom)
+    androidTestImplementation(libs.android.compose.ui.test.junit4)
+    debugImplementation(libs.android.compose.ui.tooling)
+    debugImplementation(libs.android.compose.ui.test.manifest)
 }
